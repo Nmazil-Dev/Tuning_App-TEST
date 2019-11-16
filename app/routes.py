bd2decc0947ccf5f1031f3a8dc5d1aa4fe728443
Lets-Learn-Coding <nmazil68@gmail.com>
2019-11-16 10:00:34 -0600

    Added html

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,19 @@ from flask import render_template, url_for
 
 
 @app.route('/')
-def index():
+def home():
     #user_collection = mongo.db.user
     #user_collection.insert({'name': 'new1'})
     return render_template('home.html', title='Home')
+
+@app.route('/guitar')
+def guitar():
+    return render_template('guitar.html', title='Guitar Tuner')
+    
+@app.route('/bass')
+def bass():
+    return render_template('bass.html', title='Bass Tuner')
+
+@app.route('/drums')
+def drums():
+    return render_template('drums.html', title='Drums')
