578585990fe3c9b29fc54709851baeb8c33bc2f9
Lets-Learn-Coding <nmazil68@gmail.com>
2019-11-23 16:18:39 -0600

    Added tuning dropdowns and string buttons

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://nic:pass1@cluster0-xqvdu.mongodb.net/test?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://nic:pass1@cluster0-xqvdu.mongodb.net/tuningapp?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 app = Flask(__name__)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
 from app import app, mongo
 from flask import render_template, url_for
 
+#TODO Add to utils.py and import to routes
+def get_strings(instrument, tuning):
+    instrument_col = mongo.db[instrument]
+    x = instrument_col.find({'tuning': tuning}, {"_id": 0, "tuning": 0 })
+    string_list = []
+    for doc in x:
+        strings = (doc['strings'])
+    strings = strings.values()
+    return strings
+
+def get_tunings(instrument):
+    instrument_col = mongo.db[instrument]
+    x = instrument_col.find({}, {"_id": 0, "strings": 0})
+    tunings = []
+    for doc in x:
+        tunings.append(doc['tuning'])
+    return tunings
+
 
 @app.route('/')
 def home():
@@ -10,11 +28,17 @@ def home():
 
 @app.route('/guitar')
 def guitar():
-    return render_template('guitar.html', title='Guitar Tuner')
+    tunings = get_tunings('guitar')
+    # TODO Get the tuning from the tunings dropdown and pass to get_strings()
+    strings = get_strings('guitar', "Standard")
+    return render_template('guitar.html', title='Guitar Tuner', tunings=tunings, strings=strings)
     
 @app.route('/bass')
 def bass():
-    return render_template('bass.html', title='Bass Tuner')
+    tunings = get_tunings('bass')
+    # TODO Get the tuning from the tunings dropdown and pass to get_strings()
+    strings = get_strings('bass', "Standard")
+    return render_template('bass.html', title='Bass Tuner', tunings=tunings, strings=strings)
 
 @app.route('/drums')
 def drums():
